9c059f3e10f9985cfd3c96bae46b8d8de8a26eda
Tom Hicks <hickst@tohono.com>
2018-04-24 20:21:37 -0700

    Refactor: sort, rename vars, dispatch actions, ctype mapping, handle file or directory.

--- fits.py
+++ fits.py
@@ -18,34 +18,18 @@ ALTERNATE_KEYS_MAP = {
 ALTERNATE_KEYS = set(ALTERNATE_KEYS_MAP.keys())
 DESIRED_KEYS_FILE = 'metadata-keys.txt'
 
-def filter_file_tree(dir, pattern):
-    "Generator to yield all files in the given file tree whose name matches the given pattern"
-    for root, dirs, files in os.walk(dir):
-        for file in files:
-            if (fnmatch.fnmatch(file, pattern)):
-                file_path = os.path.join(root,file)
-                yield file_path
-
-def get_desired_metadata_keys():
-    "Return a list of metadata keys to be extracted"
-    with open(DESIRED_KEYS_FILE, 'r') as mdkeys_file:
-        return mdkeys_file.read().splitlines()
-
-def handle_ctype_mapping(key, file_metadata, metadata):
-    if (key == 'CRVAL1'):
-        if 'RA' in hdu.header['CTYPE1']:
-            metadata.append( (key, str(file_metadata.get('right_ascension', ''))) )
-        elif 'DEC' in hdu.header['CTYPE1']:
-            metadata.append( (key, str(file_metadata.get('declination', ''))) )
-        else:
-            metadata.append( (key, '') )
-    elif (key == 'CRVAL2'):
-        if 'RA' in hdu.header['CTYPE2']:
-            metadata.append( (key, str(file_metadata.get('right_ascension', ''))) )
-        elif 'DEC' in hdu.header['CTYPE2']:
-            metadata.append( (key, str(file_metadata.get('declination', ''))) )
-        else:
-            metadata.append( (key, '') )
+def action_dispatch(action, fits_file):
+    "Dispatch the given FITS file to the appropriate action"
+    if (action == "info"):
+        fits_info(fits_file)
+    elif (action == "extract"):
+        metadata = fits_extract(fits_file)
+        print("METADATA(" + str(len(metadata)) + ")=" + str(metadata))    ##### REMOVE LATER
+    elif (action == "verify"):
+        fits_verify(fits_file)
+    else:
+        print("Error: Unrecognized action: '{}'".format(action))
+        sys.exit()
 
 def extract_metadata(file_path, hdu, desired_keys):
     """Extract the metadata from the HeaderDataUnit of the given file for the keys
@@ -59,8 +43,8 @@ def extract_metadata(file_path, hdu, desired_keys):
                 metadata.append( (fnorp, str(file_path)) )
             elif (key in ALTERNATE_KEYS):                   # is this an alternate key?
                 standard_key = ALTERNATE_KEYS_MAP[key]      # get more standard key
-                metadata.append( (standard_key, str(file_metadata.get(standard_key, ''))) )
-            elif (key == 'CRVAL1') or (key == 'CRVAL2'):
+                metadata.append( (key, str(file_metadata.get(standard_key, ''))) )
+            elif ((key == 'CRVAL1') or (key == 'CRVAL2')):
                 handle_ctype_mapping(key, file_metadata, metadata)
             else:                                           # just lookup the given key
                 metadata.append( (key, str(file_metadata.get(key, ''))) )
@@ -68,12 +52,20 @@ def extract_metadata(file_path, hdu, desired_keys):
             metadata.append( (key, '') )
     return metadata
 
+def filter_file_tree(dir, pattern):
+    "Generator to yield all files in the given file tree whose name matches the given pattern"
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            if (fnmatch.fnmatch(file, pattern)):
+                file_path = os.path.join(root,file)
+                yield file_path
+
 def fits_extract(file_path):
-    "Extract a dictionary of metadata from the given FITS file"
+    "Return a list of key/value metadata tuples (strings) extracted from the given FITS file"
     desired_keys = get_desired_metadata_keys()
     with fits.open(file_path) as hdu:
         metadata = extract_metadata(file_path, hdu, desired_keys)
-        print("METADATA= " + str(metadata))    ##### REMOVE LATER
+    return metadata
 
 def fits_info(file_path):
     "Print the Header Data Unit information for the given FITS file"
@@ -84,30 +76,58 @@ def fits_info(file_path):
             hdr = hdu.header
             for key in hdr.keys():
                 val = str(hdr[key])
-                if (key and val):                       # ignore blank keys or values
+                if (key and val):                   # ignore blank keys or values
                     print(key + ': ' + val)
         print()
 
-def fits_verify(file_path, problems_file):
+def fits_verify(file_path):
     """Verify that the data in the given FITS file conforms to the FITS standard.
        Writes any verification warnings to the specified problem log file.
     """
+    problems_file = "problems.txt"
     with fits.open(file_path) as hdu:
         with warnings.catch_warnings(record=True) as warns:
             hdu.verify('fix+warn')
             if (warns and len(warns) > 0):
                 with open(problems_file, 'a') as log:
-                    log.write("FILE: " + file_path)
+                    log.write("Filename: " + file_path)
                     for warn in warns:
                         log.write(str(warn.message)+'\n')
 
-def main(argv):
-    """Validate the meta-data for all FITS files in the given file tree.
+def get_desired_metadata_keys():
+    "Return a list of metadata keys to be extracted"
+    with open(DESIRED_KEYS_FILE, 'r') as mdkeys_file:
+        return mdkeys_file.read().splitlines()
+
+def handle_ctype_mapping(key, file_metadata, metadata):
+    """If key is a special value which provides the meaning of other fields, then
+       add the key and its value and then write the referenced value with a another key.
+       For CRVALs and how they relate to CRTYPEs see https://fits.gsfc.nasa.gov/fits_standard.html
     """
-    action = "info"
-    fits_pat = "*.fits"
-    problems_file = "problems.txt"
-    usage = "Usage: fits.py [-h|--help] [--info|--extract|--verify] images_directory"
+    if (key == 'CRVAL1'):
+        metadata.append( ('CRVAL1', str(file_metadata.get('CRVAL1', ''))) )
+        if 'RA' in file_metadata['CTYPE1']:
+            metadata.append( ('right_ascension', str(file_metadata.get('CRVAL1', ''))) )
+        elif 'DEC' in file_metadata['CTYPE1']:
+            metadata.append( ('declination', str(file_metadata.get('CRVAL1', ''))) )
+        else:
+            metadata.append( (key, '') )
+    elif (key == 'CRVAL2'):
+        metadata.append( ('CRVAL2', str(file_metadata.get('CRVAL2', ''))) )
+        if 'RA' in file_metadata['CTYPE2']:
+            metadata.append( ('right_ascension', str(file_metadata.get('CRVAL2', ''))) )
+        elif 'DEC' in file_metadata['CTYPE2']:
+            metadata.append( ('declination', str(file_metadata.get('CRVAL2', ''))) )
+        else:
+            metadata.append( (key, '') )
+
+
+def main(argv):
+    "Perform actions on a FITS file or a directory of FITS files."
+    action = "info"                             # default action
+    is_file = False                             # assume directory by default
+    fits_pat = "*.fits"                         # pattern for identifying FITS files
+    usage = "Usage: fits.py [-h|--help] [--info|--extract|--verify] images_path"
 
     # parse the command line arguments:
     try:
@@ -115,9 +135,10 @@ def main(argv):
     except getopt.GetoptError:
         print(usage)
         sys.exit(-1)
-    # grab the image directory path argument, if given
-    if (len(args) < 1):
-        print("Error: Missing required argument: images directory")
+
+    # check the image file or directory path argument, if given
+    if ((len(args) < 1) or (not args[0].strip())):
+        print("Error: Missing required argument: path to image file or images directory")
         print(usage)
         sys.exit(1)
 
@@ -136,20 +157,26 @@ def main(argv):
             print(usage)
             sys.exit(3)
 
-    # grab and check the image directory path argument, which must be here
-    images_dir = args[0].strip()
-    if (not images_dir or (not os.path.isdir(images_dir))):
-        print("Error: Specified images directory '{}' not found or is not a directory".format(images_dir))
-        print(usage)
-        sys.exit(1)
+    # insure that the given path refers to a readable file or valid directory
+    images_path = args[0].strip()                   # already insured non-empty above
+    if (not os.path.exists(images_path)):
+        print("Error: Specified images path '{}' not found or is not readable".format(images_path))
+        sys.exit(4)
 
-    for file in filter_file_tree(images_dir, fits_pat):
-        if (action == "info"):
-            fits_info(file)
-        elif (action == "extract"):
-            fits_extract(file)
-        elif (action == "verify"):
-            fits_verify(file, problems_file)
+    if (not os.access(images_path, os.R_OK)):
+        print("Error: Specified images path '{}' is not readable".format(images_path))
+        sys.exit(5)
+
+    if (os.path.isfile(images_path)):
+        action_dispatch(action, images_path)
+    else:
+        if (not os.path.isdir(images_path)):
+            print("Error: Specified images path '{}' is not a file or a directory".format(images_path))
+            sys.exit(5)
+        else:
+            for fits_file in filter_file_tree(images_path, fits_pat):
+                action_dispatch(action, fits_file)
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
